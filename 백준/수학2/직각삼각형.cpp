8b057ff1134876e4da0d420f034955494184f0e4
LeeJongbok <s2feeling@naver.com>
2020-02-24 22:18:11 +0900

    Create 직각삼각형.cpp

---
+++ 백준/수학2/직각삼각형.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    
+    while(1){
+        
+        int a,b,c;
+        cin >> a >> b >> c;
+        
+        if(a == 0 && b == 0 && c == 0){
+            break;
+        }
+        
+        if(c*c == a*a + b*b || a*a == b*b + c*c || b*b == a*a+c*c){
+            cout << "right" << '\n';
+        }else{
+            cout << "wrong" << '\n';
+        }
+    }
+    
+    
+    
+}
