47522b531f0a625929adca6a24d3fed35392b95c
LeeJongbok <s2feeling@naver.com>
2020-02-25 11:43:07 +0900

    Create 바이러스.cpp

---
+++ 백준/DFS&BFS/바이러스.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <cstring> 
+
+using namespace std;
+
+vector<int> a[110];
+bool check[110];
+int cnt = 0; 
+
+void dfs(int x){
+    
+    check[x] = true;
+    cnt += 1;
+    
+    for(int i=0; i<a[x].size(); i++){
+        int y = a[x][i];
+        if(check[y] == false){
+            dfs(y);
+        }
+    }
+}
+
+
+
+
+
+int main(){
+    
+    int N, C;
+    cin >> N >> C;
+    
+    for(int i=1; i<=C; i++){
+        int u, v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    
+    memset(check, false, sizeof(check));
+    
+    dfs(1);
+    
+    cout << cnt-1;
+    
+    
+}
+
+
+
+
+
+
+
+
