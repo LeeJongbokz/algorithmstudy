0f8920e66ef3dd0ab83c67787c00b350be681fac
LeeJongbok <s2feeling@naver.com>
2020-07-18 10:50:33 +0900

    Update 숫자재배치.cpp

--- 알고리즘스터디/숫자재배치.cpp
+++ 알고리즘스터디/숫자재배치.cpp
@@ -18,10 +18,12 @@ bool isExist = false;
 
 void recursive(int pos) {
 	
+	// 첫 글자가 0이면 리턴함
 	if (result[0] == '0') {
 		return;
 	}
-
+	
+	// 길이가 len이 되면 검사를 함
 	if (pos == len) {
 		int numC = stoi(result);
 		int numB = stoi(B);
@@ -35,6 +37,7 @@ void recursive(int pos) {
 	}
 
 
+	// 재귀 함수에서 백트레킹으로 result에 문자를 하나씩 더함
 	for (int i = 0; i < len; i++) {
 		if (check[i] == false) {
 			check[i] = true;
@@ -53,9 +56,13 @@ int main() {
 
 	memset(check, false, sizeof(check));
 
+	// A와 B 입력
 	cin >> A >> B;
+	
+	// A의 길이 저장
 	len = A.size();
 	
+	// 재귀 함수 진입
 	recursive(0);
 
 
