8f79ae996419125f4321e4f2aaeaee1f8328f697
LeeJongbok <s2feeling@naver.com>
2020-08-27 19:58:01 +0900

    Create 3.4 스택으로 큐(2).cpp

---
+++ 코딩인터뷰완전분석/3.4 스택으로 큐(2).cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+class Stack {
+
+	int top;
+	int data[MAX_SIZE];
+
+	public:
+
+		Stack() {
+			top = -1;
+		}
+
+		void push(int num) {
+			if (top == MAX_SIZE) {
+				cout << "Stack overflow";
+			}
+			else {
+				data[++top] = num;
+			}
+		}
+
+		int pop() {
+			if (top == -1) {
+				cout << "Stack underflow";
+			}
+			else {
+				return data[top--];
+			}
+		}
+
+		int getTop() {
+			if (top == -1) {
+				cout << "Stack underflow";
+			}
+			else {
+				return data[top];
+			}
+		}
+
+		bool empty() {
+			return top == -1;
+		}
+
+
+};
+
+
+class MyQueue {
+	Stack s1;
+	Stack s2;
+
+
+	public:
+
+	void push(int num) {
+		s1.push(num);
+	}
+
+	int pop() {
+
+		if (s2.empty()) {
+
+			while (!s1.empty()) {
+				int num = s1.getTop();
+				s1.pop();
+				s2.push(num);
+			}
+
+			int num = s2.getTop();
+			s2.pop();
+			return num;
+
+		}
+		else {
+
+			int num = s2.getTop();
+			s2.pop();
+			return num;
+		}
+
+	}
+
+
+
+
+};
+
+
+
+int main() {
+
+	MyQueue q;
+
+	q.push(100);
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	int num = q.pop();
+	cout << num;
+
+
+}
