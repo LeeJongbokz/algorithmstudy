9a16287158f7e40432f6b4e064b39df4c6b04425
LeeJongbok <s2feeling@naver.com>
2020-09-02 19:39:48 +0900

    Create 전화번호목록.cpp

---
+++ 알고리즘스터디1/프로그래머스/전화번호목록.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    
+    sort(phone_book.begin(), phone_book.end());
+    
+    int len = phone_book.size(); 
+    
+    for(int i=0; i<len-1; i++){
+        string A = phone_book[i];
+        string B = phone_book[i+1];
+        string C = B.substr(0, A.size());
+        
+        if(A == C){
+            answer = false;
+            break;
+        }    
+    }
+    
+    return answer;
+}
